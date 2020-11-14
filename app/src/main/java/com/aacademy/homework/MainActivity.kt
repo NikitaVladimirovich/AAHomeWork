9d7d7e612aeaca81fac44c4936d794ce145aa457
Nikita Balashov <n.balashov@crmsensor.ru>
2020-11-14 15:17:26 +0300

    Rename activity

--- app/src/main/java/com/aacademy/homework/MainActivity.kt
+++ app/src/main/java/com/aacademy/homework/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, DetailActivity::class.java))
+        startActivity(Intent(this, MovieDetailsActivity::class.java))
         finish()
     }
 }
--- app/src/main/java/com/aacademy/homework/MovieDetailsActivity.kt
+++ app/src/main/java/com/aacademy/homework/MovieDetailsActivity.kt
@@ -3,9 +3,9 @@ package com.aacademy.homework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class DetailActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(R.layout.activity_movie_details)
     }
 }
