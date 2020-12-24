366eae2497ea9c23d7c436d67a54b6ae30dc7151
Nikita Balashov <n.balashov@crmsensor.ru>
2020-12-24 22:22:17 +0300

    refactor: Migrate Empty/Error views from Relative to Frame

--- app/src/main/java/com/aacademy/homework/ui/views/EmptyView.kt
+++ app/src/main/java/com/aacademy/homework/ui/views/EmptyView.kt
@@ -3,14 +3,14 @@ package com.aacademy.homework.ui.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.RelativeLayout
+import android.widget.FrameLayout
 import com.aacademy.homework.databinding.LayoutEmptyBinding
 
 class EmptyView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : RelativeLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
 
     init {
         LayoutEmptyBinding.inflate(LayoutInflater.from(context), this, true)
--- app/src/main/java/com/aacademy/homework/ui/views/ErrorView.kt
+++ app/src/main/java/com/aacademy/homework/ui/views/ErrorView.kt
@@ -3,14 +3,14 @@ package com.aacademy.homework.ui.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.RelativeLayout
+import android.widget.FrameLayout
 import com.aacademy.homework.databinding.LayoutErrorBinding
 
 class ErrorView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : RelativeLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr) {
 
     private val binding = LayoutErrorBinding.inflate(LayoutInflater.from(context), this, true)
     var reloadListener: (() -> Unit)? = null
