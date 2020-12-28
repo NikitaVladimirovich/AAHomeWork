c9fe43b719f6b2ae6b569d527f22498c1f406f54
Nikita Balashov <n.balashov@crmsensor.ru>
2020-12-28 14:20:41 +0300

    refactor: One line functions in LocalSourceImpl

--- app/src/main/java/com/aacademy/homework/data/local/LocalSourceImpl.kt
+++ app/src/main/java/com/aacademy/homework/data/local/LocalSourceImpl.kt
@@ -7,24 +7,20 @@ import javax.inject.Inject
 
 class LocalSourceImpl @Inject constructor(private val database: AppDatabase) : LocalSource {
 
-    override suspend fun getAllMoviePreviews(): List<MoviePreviewWithGenres> {
-        return database.moviePreviewDao().getAllMovies()
-    }
+    override suspend fun getAllMoviePreviews(): List<MoviePreviewWithGenres> =
+        database.moviePreviewDao().getAllMovies()
 
-    override suspend fun getMovieDetail(id: Long): List<MovieDetailWithActors> {
-        return database.movieDetailDao().getMovieDetail(id)
-    }
+    override suspend fun getMovieDetail(id: Long): List<MovieDetailWithActors> =
+        database.movieDetailDao().getMovieDetail(id)
 
-    override suspend fun setMovieLiked(id: Long, isLiked: Boolean) {
-        return database.moviePreviewDao().setMovieLiked(id, isLiked)
-    }
+    override suspend fun setMovieLiked(id: Long, isLiked: Boolean) =
+        database.moviePreviewDao().setMovieLiked(id, isLiked)
 
     override suspend fun cacheMoviePreviewsWithGenres(moviePreviewsWithGenres: List<MoviePreviewWithGenres>) {
         database.moviePreviewDao().clear()
         database.moviePreviewDao().insert(moviePreviewsWithGenres)
     }
 
-    override suspend fun cacheMovieDetailWithActors(movieDetailWithActors: MovieDetailWithActors) {
+    override suspend fun cacheMovieDetailWithActors(movieDetailWithActors: MovieDetailWithActors) =
         database.movieDetailDao().insert(movieDetailWithActors)
-    }
 }
