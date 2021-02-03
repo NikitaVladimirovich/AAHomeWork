f7eee7ee97f643024c38f523f4b097e06e51cc05
Nikita Balashov <n.balashov@crmsensor.ru>
2021-02-03 23:15:09 +0300

    fix: ktlint

--- app/src/androidTest/java/com/aacademy/homework/di/FakeAppModule.kt
+++ app/src/androidTest/java/com/aacademy/homework/di/FakeAppModule.kt
@@ -10,7 +10,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
-
 import kotlinx.serialization.ExperimentalSerializationApi
 
 @ExperimentalSerializationApi
